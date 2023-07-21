981501f5ec5c035e9f56881ae1a83a29efa21d8c
Favour Okechukwu <favourapps17@gmail.com>
2023-07-21 21:26:02 +0100

    added model and migration file for friends

---
+++ database/migrations/2023_07_21_201536_create_friends_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('userid_1');
+            $table->unsignedBigInteger('userid_2');
+            $table->timestamps();
+
+            $table->foreign('userid_1')->references('id')->on('users');
+            $table->foreign('userid_2')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('friends');
+    }
+};
